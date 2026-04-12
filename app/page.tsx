18d40d27720c459cabe7e6213d0df290795ef51c
RiseMantis <neelsankhe2007@gmail.com>
2026-04-12 19:05:12 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import './globals.css'
+
+export default function Home() {
+  return (
+    <main id='home-container'>
+      <h1 id='main-heading'>
+        Meowies
+      </h1>
+
+      <div id='go-ahead'>
+        play
+      </div>
+    </main>
+  );
+}
